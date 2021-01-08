154a97c4a500fce10c23d3d2cd384a8b9eff5299
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2021-01-08 11:04:40 -0500

    updating user controller and secure user route

--- server/routes/secure/users.js
+++ server/routes/secure/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router(),
-  { getCurrentUser } = require('../../controllers/users');
+  { getCurrentUser, updateCurrentUser } = require('../../controllers/users');
 
 router.get('/me', getCurrentUser);
+router.patch('/me', updateCurrentUser);
 
 module.exports = router;
