1f407092e8a4a78a0c7703348d12a83c68d20d60
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2021-01-19 23:16:59 -0500

    basic dashboard up with blank tabs

---
+++ client/src/components/events/MyEvents.jsx
@@ -0,0 +1,75 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import moment from 'moment';
+// import '../components/MyEvents.css';
+
+// const MyEvents = () => {
+//   const [events, setEvents] = useState(null);
+//   const [isUpdated, setIsUpdated] = useState(false);
+
+//   const getEvents = async () => {
+//     try {
+//       let res = await axios({
+//         method: 'GET',
+//         url: `/api/events`
+//       });
+//       setEvents(res.data);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   useEffect(() => {
+//     getEvents();
+//   }, [isUpdated]);
+
+//   return (
+//     <div
+//       className="saved-event-area"
+//       // style={{ marginTop: '4rem' }}
+//     >
+//       {events?.map((event1) => {
+//         return (
+//           <div className="saved-event-card-area">
+//             <div
+//               className="flex p-2 m-4 w-full justify-center saved-event-inner-card-area"
+//               style={{
+//                 borderTopRightRadius: '0.5rem',
+//                 borderBottomRightRadius: '0.5rem',
+//                 backgroundColor: '#FFF7F1',
+//                 marginBottom: '.4rem',
+//                 borderLeft: '8px solid #A6271F',
+//                 maxWidth: '700px',
+//                 padding: '20px',
+//                 textAlign: 'center'
+//               }}
+//             >
+//               <div className="saved-event-inner-card-text-area">
+//                 <h2 className="dash-h2">{event1?.eventTitle}</h2>
+//                 <br />
+//                 <span className="saved-event-card-custom-date">
+//                   {moment(event1?.eventDate).format('MMMM Do YYYY')}
+//                 </span>
+//                 <br />
+//                 {
+//                   <span
+//                     className="saved-event-has-been-opened-text"
+//                     style={
+//                       event1?.hasBeenOpened
+//                         ? { color: 'green', fontWeight: '600' }
+//                         : { color: 'red', fontWeight: '600' }
+//                     }
+//                   >
+//                     {event1?.hasBeenOpened ? 'Opened by Artist' : 'Not Opened'}
+//                   </span>
+//                 }
+//                 <br />
+//               </div>
+//               <div className="saved-event-card-last"></div>
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+// export default MyEvents;
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import MyStages from '../components/stages/MyStages';
+// import NewStage from '../components/stages/NewStage';
+import MyEvents from '../components/events/MyEvents';
+// import CreateEvent from '../components/events/createEvent/CreateEvent';
+import MyEquipment from '../components/equipment/MyEquipment';
+// import StageDetails from '../components/stages/StageDetails';
+// import AddImage from '../components/AddImage';
+import '../pages/Dashboard.css';
+
+class Dashboard extends React.Component {
+  state = { show: false, image: null, preview: null, currentTab: 'equip' };
+
+  render() {
+    return (
+      <div className='profile-container'>
+        <div
+          className='header-container'
+          style={{
+            backgroundImage: "url('../dashboard-images/jennyblock2.png')",
+            height: '36vh',
+          }}
+        >
+          <div className='profile-picture'>{/* <AddImage /> */}</div>
+
+          {/* <CreateEvent show={this.state.show} /> */}
+        </div>
+        <BrowserRouter>
+          <div className='nav-div'>
+            <Link
+              to='/dashboard/events'
+              onClick={() => this.setState({ currentTab: 'events' })}
+              className={`dash-tab ${
+                this.state.currentTab === 'events'
+                  ? 'selected-tab'
+                  : 'unselected-tab'
+              }`}
+            >
+              My Events
+            </Link>
+            <Link
+              to='/dashboard/stages'
+              onClick={() => this.setState({ currentTab: 'stages' })}
+              className={`stage-tab ${
+                this.state.currentTab === 'stages'
+                  ? 'selected-tab'
+                  : 'unselected-tab'
+              }`}
+              style={{
+                margin: '0px',
+                color: '#fff7f1',
+                fontWeight: '600',
+              }}
+            >
+              My Stages
+            </Link>
+            <Link
+              to='/dashboard/equipment'
+              onClick={() => this.setState({ currentTab: 'equip' })}
+              className={`equip-tab ${
+                this.state.currentTab === 'equip'
+                  ? 'selected-tab'
+                  : 'unselected-tab'
+              }`}
+            >
+              My Equipment{' '}
+            </Link>
+          </div>
+
+          {/* <Switch>
+            <Route exact path="/dashboard/events" component={MyEvents} />
+            <Route exact path="/dashboard/new-stage" component={NewStage} />
+            <Route
+              exact
+              path="/dashboard/stages/:id"
+              component={StageDetails}
+            />
+            <Route exact path="/dashboard/stages" component={MyStages} />
+            <Route exact path="/dashboard/equipment" component={MyEquipment} />
+          </Switch> */}
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
