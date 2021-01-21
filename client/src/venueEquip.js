ab723ec9e3f460bc40b9cc7621c72e27067d048c
Joelle <71191459+JoelleOrr@users.noreply.github.com>
2021-01-21 18:50:08 -0500

    Dashboard 1 AND 2 with MyEquipment added

---
+++ client/src/venueEquip.js
@@ -0,0 +1,60 @@
+module.exports = [
+  [
+    { name: 'XLR Cable', iconURL: 'https://source.unsplash.com/random/80x80' },
+    {
+      name: 'Quarter-Inch Cable',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+  ],
+  [
+    { name: 'Mixer', iconURL: 'https://source.unsplash.com/random/80x80' },
+    { name: 'CDJ', iconURL: 'https://source.unsplash.com/random/80x80' },
+    {
+      name: 'Vinyl Player',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+    {
+      name: 'Other DJ Equipment',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+  ],
+  [
+    {
+      name: 'Wired Microphone',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+    {
+      name: 'Wireless Microphone',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+  ],
+  [
+    { name: 'Monitor', iconURL: 'https://source.unsplash.com/random/80x80' },
+    {
+      name: 'House Sound',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+  ],
+  [
+    { name: 'Mic Stand', iconURL: 'https://source.unsplash.com/random/80x80' },
+    {
+      name: 'Guitar Stand',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+    {
+      name: 'Keyboard Stand',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+    {
+      name: 'Other Stand',
+      iconURL: 'https://source.unsplash.com/random/80x80',
+    },
+  ],
+  [{ name: 'Passive DI Box' }, { name: 'Active DI Box' }],
+  [
+    { name: 'Drum Kit' },
+    { name: 'Keyboard' },
+    { name: 'Piano' },
+    { name: 'Other Instrument' },
+  ],
+];
