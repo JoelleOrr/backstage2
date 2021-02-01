e703f3e1f8cb7dda848e5b4acf044ed8e4593e17
Rita Mwaikinda <srmwaikinda@gmail.com>
2021-01-31 23:50:43 -0500

    first page - Ready to experience?

---
+++ client/src/components/onboarding/Ready.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ready.css';
+
+function Ready() {
+  return (
+    <div>
+      <div className='readyPage'>
+        <div className='readyTitle'>Ready to experience a new way to book?</div>
+        <div className='doItButton'>lets do it</div>
+        <div className='notYetButton'>not yet</div>
+      </div>
+    </div>
+  );
+}
+
+export default Ready;
